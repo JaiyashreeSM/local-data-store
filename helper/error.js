f922845a8fee4751328f230e2be5be49581a4675
Jaiyashree Subramanian <jaiyashreesm@gmail.com>
2020-02-26 23:00:59 +0530

    fix: concurrency test and fixes

--- helper/error.js
+++ helper/error.js
@@ -8,7 +8,11 @@
  * @author Jaiyashree Subramanian
  */
 const handleError = (err, res) => {
+  console.log(err)
   if(err) {
+    if(!err.statusCode) {
+      err = { statusCode: 500, message: err.message };
+    }
     res.status(err.statusCode).json({
       status: "error",
       statusCode : err.statusCode,
